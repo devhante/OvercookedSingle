37c8d2d8b081a08ddafcbc041b9f19c35833c775
Yujun Cho <devhante@gmail.com>
2025-03-29 14:45:00 +0900

    플레이어 움직임 구현

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    
+    [SerializeField] private Player player;
+    
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IsWalking, player.IsWalking);
+    }
+}
