a648265841e7c48d23ca17a70b3887064468a18d
Yujun Cho <devhante@gmail.com>
2025-03-29 16:31:03 +0900

    InputSystem 적용

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions _playerInputActions;
+    
+    private void Awake()
+    {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+        
+        inputVector = inputVector.normalized;
+        
+        return inputVector;
+    }
+}
